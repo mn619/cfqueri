b59c3d6bd82b0d7856eeaed415e584e090997221
mn619 <amandubey961@gmail.com>
2020-05-11 12:50:41 +0530

    remove duplicate problems from result

--- src/App.js
+++ src/App.js
@@ -4,22 +4,65 @@ import Form from './Form';
 
 class App extends Component {
  state ={
-        handle: null,
-        tag: null,
+ 		query:[{
+        	handle: null,
+        	tag: null,
+        }],
+        problems: []
     }
     addQuery = (e) =>{
-    	this.setState(e);
-    }
-    
-  render(){
-    return (
-      <div className="App">
-      <Form getQuery = {this.addQuery}/>
-      <br/>
-      <Background query = {this.state} />
-      </div>
-    );
-  }
+    	this.setState({query: e});
+    	
+    	var handle = e.handle;
+    	var tag = e.tag;
+		var problems = [];
+    	var apiAddress = 'https://codeforces.com/api/user.status?handle=' + handle;
+
+		fetch(apiAddress)
+		.then(results => {
+		    return results.json();
+		}).then(data => {
+	    	problems = data.result.map((problem) => {
+				var contestId = problem['problem']['contestId'] !== undefined? problem['problem']['contestId'].toString(): 0
+				var problemId = problem['problem']['contestId'] !== undefined? problem['problem']['contestId'].toString() + problem['problem']['index'].toString(): 0;
+				var problemName = problem['problem']['name'];
+				var problemIndex = problem['problem']['index'].toString();
+				var problemRating = problem['problem']['rating'] !== undefined? problem['problem']['rating'].toString(): -1;
+
+				var link = 'http://codeforces.com/problemset/problem/' + contestId + "/" + problemIndex;
+			   	if(problem['verdict'] === 'OK' && ('tags' in problem['problem']) && (problem['problem']['tags'].includes(tag))){
+			   		return ({contestId: contestId, problemId : problemId, problemName: problemName, problemIndex: problemIndex, problemRating: problemRating, problemLink: link});
+			   	}
+			   	else return null;
+			})
+
+	    	//filter the null elements
+	        problems = problems.filter( function (obj){
+	        	return obj  !== null;
+	        });
+	        
+	        var uniqueProblems = [];
+	        problems.forEach(problem => {
+			  if (!uniqueProblems.some(p => p.problemId === problem.problemId)) {
+				uniqueProblems.push({ ...problem })
+			  }
+			});
+			
+			problems = uniqueProblems;
+			
+	        this.setState({problems: problems});
+		})
+	}
+	
+	render(){
+		return (
+			<div className="App">
+			<Form  getQuery={this.addQuery} />
+			<br/>
+			<Background problems={this.state.problems} query={this.state.query} />
+			</div>
+		)
+	}
 }
 
 export default App;
--- src/Background.js
+++ src/Background.js
@@ -1,73 +1,35 @@
 import React, { Component } from 'react';
 
 class Background extends Component{
-constructor(){
-    super();
-    this.state = {
-        query: [],
-        problems: [],
-    };
+constructor(props, context){
+    super(props, context);
 }
 
-
-componentDidUpdate(prevProps){
-    var handle = this.props.query.handle;
-    var tag = this.props.query.tag;
-
-    var apiAddress = 'https://codeforces.com/api/user.status?handle=' + handle; 
-	
-    fetch(apiAddress)
-    .then(results => {
-        return results.json();
-    }).then(data => {
-        // console.log(data.result);
-        
-        let problems = data.result.map((problem) => {
-            var contestId = problem['problem']['contestId'] !== undefined? problem['problem']['contestId'].toString(): 0
-            var problemId = problem['problem']['contestId'] !== undefined? problem['problem']['contestId'].toString() + problem['problem']['index'].toString(): 0;
-            var problemName = problem['problem']['name'];
-            var problemIndex = problem['problem']['index'].toString();
-            var problemRating = problem['problem']['rating'] !== undefined? problem['problem']['rating'].toString(): -1;
-
-            var link = 'http://codeforces.com/problemset/problem/' + contestId + "/" + problemIndex;
-            if(problem['verdict'] === 'OK' && ('tags' in problem['problem']) && (problem['problem']['tags'].includes(tag)))
-            return(
-                <div key = {problem.id} className = "list-group">
-                    <ul className ="list-group list-group-horizontal">
-                      <a href = {link}  style={{width:"50%"}}>
-                          <li className ="list-group-item">
-                            {problemId} {problemName}
-                          </li>
-                      </a>
-                      <li className ="list-group-item" style={{width:"80px"}}>
-                        {problemRating}
-                      </li>
-                    </ul>
-                </div>
-            )
-            else return null;
-        })
-        
-        //filter the null elements
-        problems = problems.filter( function (obj){
-        	return obj  !== null;
-        });
-        
-        //if the query change, we update current state
-        if(prevProps.query !== this.props.query)
-        this.setState({query: this.props.query})
-        this.setState({problems: problems});
-    })
-}
 
 render() {
+	let problems = this.props.problems.map((problem) => {
+	    return(
+	        <div key = {problem.problemId} className = "list-group">
+	            <ul className ="list-group list-group-horizontal">
+	              <a href = {problem.problemLink}  style={{width:"50%"}}>
+	                  <li className ="list-group-item">
+	                    {problem.problemId} {problem.problemName}
+	                  </li>
+	              </a>
+	              <li className ="list-group-item" style={{width:"80px"}}>
+	                {problem.problemRating}
+	              </li>
+	            </ul>
+	        </div>
+	    )
+	})
     return (
+            <div>
         <div className="container1">
-            <div class="alert alert-dark" role="alert">
-              Handle : {this.state.query.handle} ProblemTag : {this.state.query.tag}
+              Handle : {this.props.query.handle} ProblemTag : {this.props.query.tag}
             </div>
             <div className="container3">
-                {this.state.problems}
+                {problems}
             </div>
         </div>
     )
